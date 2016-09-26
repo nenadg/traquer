6e8faa759fe38ed0f4a5ca6785e12bdc9cb641ca
nenadg <nenadgolubovic@gmail.com>
2016-09-26 07:52:44 +0200

    - initial timeline

--- public/lib/controls.js
+++ public/lib/controls.js
@@ -18,12 +18,11 @@ var Controls = function(){
     this.traquer = new Traquer();
 }
 
-Controls.prototype.recording = function(controls, e) {
+Controls.prototype.recording = function(controls, e){
 	var self     = controls,
 		traquer  = self.traquer,
 		playstop = e.target;
 	
-
 	if(!traquer.isRecording){
 		traquer.start();
 		playstop.className = 'playstop squared';
@@ -32,5 +31,43 @@ Controls.prototype.recording = function(controls, e) {
 	
 	playstop.className = 'playstop';
 	traquer.stop();
-	traquer.play(traquer.recordedEvents)
+
+	self.timeline.call(self);
+	//traquer.play(traquer.recordedEvents)
+}
+
+Controls.prototype.timeline = function(){
+	var self     = this,
+		traquer  = self.traquer,
+		events   = traquer.recordedEvents;
+
+	var timelineContainer       = document.createElement('ol');
+	timelineContainer.className = 'timeline-container'; 
+	document.body.appendChild(timelineContainer);
+
+	var tlcBox   = timelineContainer.getBoundingClientRect(),
+		tlcWidth = tlcBox.width,
+		last     = events[events.length -1].time;
+
+
+	var i;
+	for (i in events){
+		if(events.hasOwnProperty(i)){
+			var event             = events[i],
+				percentInTime     = parseInt(event.time / last * 100),
+				percentInTimeLine = parseInt(percentInTime * tlcWidth / 100),
+				eventConstructor  = ['<li style="left: ' + percentInTimeLine + 'px;">',
+								    	'<span class="tl-type">',
+								    		'{title}',
+								    	'</span><br/>',
+								    	'<span class="tl-selector">',
+								    		'{selector}',
+								    	'</span>',
+								  	'</li>'].join('');
+			
+			eventConstructor = eventConstructor.replace(/{title}/igm, event.type)
+											   .replace(/{selector}/igm, event.selector);
+			timelineContainer.innerHTML += eventConstructor;
+		}
+	}
 }
